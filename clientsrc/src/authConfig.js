bea5641bd533feda09987c65485c270f93e11cf9
CoreyWhitmore <cjwhit97@hotmail.com>
2020-09-28 09:22:55 -0600

    first commit

---
+++ clientsrc/src/authConfig.js
@@ -0,0 +1,3 @@
+export const domain = "corey101997.us.auth0.com";
+export const audience = "https://bug-log";
+export const clientId = "JvgvouRLU0mtpHoE0HDtyYgh9pne6821";
---
+++ server/services/BugsService.js
@@ -0,0 +1,44 @@
+import { dbContext } from "../db/DbContext"
+import { BadRequest } from "../utils/Errors"
+
+
+class BugService {
+    async getAll() {
+        return await dbContext.Bugs.find({}).populate("creator", "name picture")
+    }
+
+    async getById(id) {
+        let data = await dbContext.Bugs.findOne({ _id: id })
+        if (!data) {
+            throw new BadRequest("Invalid ID or you do not own this bug")
+        }
+        return data
+    }
+    async getNotes(bugId) {
+        return await dbContext.Notes.find({ bugId: bugId }).populate("creator", "name picture")
+    }
+
+    async create(rawData) {
+        let data = await dbContext.Bugs.create(rawData)
+        return data
+    }
+
+    async edit(id, userEmail, update) {
+        let data = await dbContext.Bugs.findOneAndUpdate({ _id: id, creatorEmail: userEmail }, update, { new: true })
+        if (!data) {
+            throw new BadRequest("Invalid ID or you did not post this bug");
+        }
+        return data;
+    }
+
+    async delete(id, userEmail) {
+        let data = await dbContext.Bugs.findOneAndRemove({ _id: id, creatorEmail: userEmail });
+        if (!data) {
+            throw new BadRequest("Invalid ID or you did not post this bug");
+        }
+    }
+
+}
+
+
+export const bugService = new BugService()
